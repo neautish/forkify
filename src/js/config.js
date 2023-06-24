401f14c7560caf299bf683be4a24943f38651878
Navab Rahimi <75166589+neautish@users.noreply.github.com>
2023-06-24 12:29:09 +0330

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
-export const API_KEY = process.env.API_KEY;
-export const API_URL = process.env.API_URL;
+export const API_KEY = process.env.REACT_APP_API_KEY;
+export const API_URL = process.env.REACT_APP_API_URL;
 export const TIMEOUT_SEC = 10;
 export const RES_TO_IGNORE = 10;
 export const RES_PER_PAGE = 10;
